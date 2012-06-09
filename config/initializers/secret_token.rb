c4170476509213516c49452310a94582cff3d98b
John Philip Dorado <dorado.johnphilip@gmail.com>
2012-06-09 09:15:35 +0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ninetofive::Application.config.secret_token = '0a46d464a41c8f597d64193a5ae64c500016fd66634f04e14af9413615cc5bad4cb7b6ef03f0ec2b05d3265cf7e0003a36c40746c951434534af3dbc972e2399'
