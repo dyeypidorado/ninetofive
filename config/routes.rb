8c911f6cc6a39396e23104f02d97c0a366656b06
Loudcloud <loudcloud@office.loudcloud.ph>
2012-06-13 21:50:56 +0800

    routing conf

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,20 @@
 Ninetofive::Application.routes.draw do
+  devise_for :admins
+
+  namespace :admin do
+    resources :products do
+      resources :subscribers
+    end
+  end
+
   resources :products do
     resources :subscribers
   end
 
-  root :to => 'products#index'
+  match 'products/:product_id/subscriber/:id/confirm' => 'subscribers#confirm_subscription' ,:as => :confirm
+  match '/dashboard' => 'Admin::products#index', :as => :admin_root
+
+  root :to => 'home#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
