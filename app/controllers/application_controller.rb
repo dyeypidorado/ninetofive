a715a9b9bc71dddb73d081d680c32a3371ab5053
Rystraum Gamonez <rystraum@gmail.com>
2012-06-14 15:09:04 +0800

    moved set_madmimi to separate module

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def set_madmimi
-    if Rails.env.production?
-      @mimi = MadMimi.new('leilahernandez@9to5mil.com', 'b4696760ebc6fe479762d366a04223e1')
-    else
-      @mimi = MadMimi.new('rystraum@gmail.com', 'd14980145460d10a304b52bfc973c1f3')
-    end
+    Mimi::set_madmimi
   end
 end
 
---
+++ config/initializers/mimi.rb
@@ -0,0 +1,11 @@
+module Mimi
+  def self.set_madmimi
+    if Rails.env.production?
+      @mimi = MadMimi.new('leilahernandez@9to5mil.com', 'b4696760ebc6fe479762d366a04223e1')
+    else
+      @mimi = MadMimi.new('rystraum@gmail.com', 'd14980145460d10a304b52bfc973c1f3')
+    end
+    return @mimi
+  end
+end
+
