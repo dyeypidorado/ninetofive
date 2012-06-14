bcbaf1c6a7653724ac284bb957e79d90aac75b24
Rystraum Gamonez <rystraum@gmail.com>
2012-06-14 12:24:05 +0800

    mailcatcher settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,19 +34,11 @@ Ninetofive::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
-  
+
   #Mailer settings
   #SMTP
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :domain               => "gmail.com",
-  :user_name            => "dorado.johnphilip",
-  :password             => "judorado_25",
-  :authentication       => "plain",
-  :enable_starttls_auto => true
-  }
+  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
 end
+
