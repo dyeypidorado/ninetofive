b9463b5b74d593506b82a3e7669c36bcbf5a16dc
Rystraum Gamonez <rystraum@gmail.com>
2012-06-14 14:48:35 +0800

    production settings for madmimi api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def set_madmimi
-    @mimi = MadMimi.new('rystraum@gmail.com', 'd14980145460d10a304b52bfc973c1f3')
+    if Rails.env.production?
+      @mimi = MadMimi.new('leilahernandez@9to5mil.com', 'b4696760ebc6fe479762d366a04223e1')
+    else
+      @mimi = MadMimi.new('rystraum@gmail.com', 'd14980145460d10a304b52bfc973c1f3')
+    end
   end
 end
 
