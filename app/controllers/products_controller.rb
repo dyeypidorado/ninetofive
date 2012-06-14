c62524df4d14862bdef67f64abf75e0ac10b5d65
Rystraum Gamonez <rystraum@gmail.com>
2012-06-14 10:18:12 +0800

    default to Product.first on products#show if no id was passed

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,5 +3,9 @@ class ProductsController < ApplicationController
   end
 
   def show
+    @product = Product.first unless params[:id]
+    @product = Product.find(params[:id]) if params[:id]
+    @subscriber = @product.subscribers.build
   end
 end
+
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,14 +1,19 @@
 class Product < ActiveRecord::Base
   attr_accessible :listname, :name
-  
+
   extend FriendlyId
   friendly_id :name, use: :slugged
-  
+
   has_many :lists, :foreign_key => :product_id, :dependent => :destroy
   has_many :subscribers, :through => :lists, :source => :subscriber, :dependent => :destroy
-  
+
   validates :name,
             :presence => true
   validates :listname,
             :presence => true
+  def to_s
+    "#{name}"
+  end
+
 end
+
