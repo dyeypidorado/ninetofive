6d081f50d6492faf14860442053a389e70d87dd3
Loudcloud <loudcloud@office.loudcloud.ph>
2012-06-22 10:46:36 +0800

    Pages Controller for users[subscribers]

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+  def show
+    @page = Page.find_by_link_code(params[:page_id]) or Page.find_by_link_code(params[:id])
+    @product = @page.product
+
+    if @source
+      @subscriber = Subscriber.find_by_email(@source)
+    end
+  end
+end
