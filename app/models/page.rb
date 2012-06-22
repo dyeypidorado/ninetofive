2288731da7fc317d09a22f9840e6227695ee92d0
Loudcloud <loudcloud@office.loudcloud.ph>
2012-06-22 10:53:03 +0800

    added generate_link feature to Page model

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,4 +1,13 @@
 class Page < ActiveRecord::Base
-  attr_accessible :content, :link_code, :status_code, :title
+  attr_accessible :content, :status_code, :title
+  attr_readonly :link_code
+
   belongs_to :product
+  has_one :status
+
+  before_create :generate_code
+
+  def generate_code
+    self.link_code = rand(36**12).to_s(36)
+  end
 end
