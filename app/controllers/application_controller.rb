8936d166ad5798525c7ff0bbbcc521628bafd9d5
Loudcloud <loudcloud@office.loudcloud.ph>
2012-06-22 20:14:07 +0800

    created "save_referrer" and "get_source" functions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :save_referrer
+  before_filter :get_source
 
   def set_madmimi
     @mimi = Mimi::set_madmimi
   end
-end
 
+  def save_referrer
+    if params[:referrer]
+      Referral.create(code: params[:referrer], link: request.url)
+      @referrer = params[:referrer]
+    end
+  end
+
+  def get_source
+    @source = params[:source] if params[:source]
+  end
+end
