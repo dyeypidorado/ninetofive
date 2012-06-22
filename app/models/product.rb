cc9363c4c38c4d70b7137322a259b879750aeb1c
Loudcloud <loudcloud@office.loudcloud.ph>
2012-06-22 23:04:09 +0800

    product model automatically create squeeze page

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,21 +1,21 @@
 class Product < ActiveRecord::Base
   attr_accessible :name #, :listname
-  after_create :create_mimi_list
+  after_create :create_squeeze
 
   extend FriendlyId
   friendly_id :name, use: :slugged
 
-  has_many :lists
-  has_many :subscribers, :through => :lists
-
   has_many :pages
+  has_many :lists, :through => :pages
 
   validates :name, presence: true
   def to_s; name end
-  def listname; name end
 
-  def create_mimi_list
-    @mimi = Mimi::set_madmimi
-    @mimi.new_list listname
+  def create_squeeze
+    self.pages.create title: "Your squeeze page title here.", content: "Your content here." , page_type: PageType::Squeeze
+  end
+
+  def subscribers_list (page)
+    page.subscribers
   end
 end
