00b1528615c9a7e9922771c5a843a59c3255d7db
Rystraum Gamonez <rystraum@gmail.com>
2012-06-24 23:42:05 +0800

    changed confirmation mailer to use page instead of product

--- app/mailers/confirmation_mailer.rb
+++ app/mailers/confirmation_mailer.rb
@@ -1,9 +1,9 @@
 class ConfirmationMailer < ActionMailer::Base
   default from: "no-reply@9to5mil.com"
 
-  def send_confirmation(product,subscriber)
+  def send_confirmation(page, subscriber)
     @subscriber = subscriber
-    @product = product
+    @code = subscriber.lists.find_by_page_id(page.id).confirmation_code
     mail(:to => subscriber.email, :subject => "Confirmation Message")
   end
 end
