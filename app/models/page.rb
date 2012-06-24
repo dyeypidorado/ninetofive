58c4cc7c01bf2721ba7a81a4d5f1765865e7acb5
Rystraum Gamonez <rystraum@gmail.com>
2012-06-24 23:41:29 +0800

    changed listname format

--- app/models/page.rb
+++ app/models/page.rb
@@ -2,23 +2,21 @@ class Page < ActiveRecord::Base
   attr_accessible :content, :status_code, :title, :page_type
   attr_readonly :link_code
 
-  after_create :create_mimi_list
-
-  belongs_to :product
-
   has_one :list
   has_many :subscribers, :through => :list
+  belongs_to :product
 
   before_create :generate_code
+  after_create :create_mimi_list
 
   def pagetype
     case page_type
     when PageType::Squeeze
-      "squeeze"
+      "Squeeze"
     when PageType::Sales
-      "sales"
+      "Sales"
     else
-      "-"
+      ""
     end
   end
 
@@ -26,7 +24,7 @@ class Page < ActiveRecord::Base
     self.link_code = rand(36**12).to_s(36)
   end
 
-  def listname; "#{product.name} - #{title}" end
+  def listname; "#{product.name}-#{id}-#{pagetype}" end
   def create_mimi_list
     @mimi = Mimi::set_madmimi
     @mimi.new_list listname
