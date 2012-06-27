62aef4e08a5e906772ae724a0346fa44daad026a
Loudcloud <loudcloud@office.loudcloud.ph>
2012-06-27 19:35:22 +0800

    Seeded first 10 affiliates

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,17 @@ password = 'admin_9to5'
 Admin.create(email: 'admin@9to5mil.com', password: password, :password_confirmation => password)
 Product.create name: "The 9 to 5 Millionaire Book"
 
+Affiliate.create(name: "Affiliate1")
+Affiliate.create(name: "Affiliate2")
+Affiliate.create(name: "Affiliate3")
+Affiliate.create(name: "Affiliate4")
+Affiliate.create(name: "Affiliate5")
+Affiliate.create(name: "Affiliate6")
+Affiliate.create(name: "Affiliate7")
+Affiliate.create(name: "Affiliate8")
+Affiliate.create(name: "Affiliate9")
+Affiliate.create(name: "Affiliate10")
+
 =begin
 statuses = ["Pending", "Confirmed", "Bought"]
 
