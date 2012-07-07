b1ab27e8a5d066f89e7389c05bc53770c6e28950
Rystraum Gamonez <rystraum@gmail.com>
2012-07-08 00:37:06 +0800

    moved model calls from view to application controller, added id and class to body so we can target css based on controller and action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :save_referrer
   before_filter :get_source
+  before_filter :get_pages
 
   def set_madmimi
     @mimi = Mimi::set_madmimi
@@ -24,4 +25,13 @@ class ApplicationController < ActionController::Base
   def get_source
     @source = Subscriber.find_by_email params[:source] if params[:source]
   end
+
+  def get_pages
+    @book = Category.find_by_name("Books")
+    @merchandise = Category.find_by_name("Merchandise")
+    @seminars = Category.find_by_name("Seminars")
+    @news = StaticPage.find_by_name("News")
+    @contact = StaticPage.find_by_name("Contact")
+  end
 end
+
