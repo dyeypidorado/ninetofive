6c18bf23b23cf28b2dec19f7a936d4cea6a9bc9d
Loudcloud <loudcloud@office.loudcloud.ph>
2012-07-10 03:57:07 +0800

    Campaign will generate trigger_code before create

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,6 +1,14 @@
 class Campaign < ActiveRecord::Base
-  attr_accessible :name, :trigger_code
+  attr_accessible :name, :trigger_code, :steps_attributes
 
   belongs_to :product
   has_many :steps
+
+  before_create :generate_code
+
+  accepts_nested_attributes_for :steps, :reject_if => lambda { |a| a[:promotion_name].blank? }, :allow_destroy => true
+
+  def generate_code
+    self.trigger_code = rand(36**12).to_s(36)
+  end
 end
