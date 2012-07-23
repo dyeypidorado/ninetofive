dc9e64d5166c3a5b62098670446b0641adba4583
Loudcloud <loudcloud@office.loudcloud.ph>
2012-07-23 19:42:38 +0800

    rspec, test gemfiles

--- Gemfile
+++ Gemfile
@@ -21,6 +21,19 @@ group :assets do
   gem 'uglifier', '>= 1.0.3'
 end
 
-group :development do
+group :development, :test do
   gem 'pry'
+  gem 'rspec-rails'
+  gem 'factory_girl_rails'
+end
+
+group :test do
+  gem 'spork'
+  gem 'database_cleaner'
+  gem 'guard-rspec'
+  gem 'guard-rake'
+  gem 'guard-spork'
+  gem 'rb-inotify'
+  gem 'libnotify'
+  gem 'ffaker'
 end
--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -12,3 +12,16 @@ class Campaign < ActiveRecord::Base
     self.trigger_code = rand(36**12).to_s(36)
   end
 end
+
+# describe Campaign do
+#   it { should respond_to(:name) }
+#   it { should respond_to(:trigger_code) }
+#   context ".trigger_code" do
+#     subject { @campaign.trigger_code }
+#     it { should be_kind_of(String) }
+#     @campaign = Campaign.create name: "blah"
+#     @campaign1 = Campaign.create name: "asd"
+#     specify { @campaign.trigger_code.should_not be_nil }
+#     specify { @campaign.trigger_code.should_not == @campaign1.trigger_code }
+#   end
+# end
